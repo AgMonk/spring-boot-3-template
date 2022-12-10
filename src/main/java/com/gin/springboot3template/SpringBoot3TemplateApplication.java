f5c84e966361802e55bbdc7c5766d4b7abc35585
ginstone <hjg719@139.com>
2022-12-10 10:45:52 +0800

    初始化 - 配置

---
+++ src/main/java/com/gin/springboot3template/SpringBoot3TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.gin.springboot3template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3TemplateApplication.class, args);
+    }
+
+}
