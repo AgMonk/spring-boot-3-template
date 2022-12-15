ff9bfe2eef458245ab14f9734e49adde266b4539
ginstone <hjg719@139.com>
2022-12-15 17:27:49 +0800

    授权管理框架 - 修

--- src/main/java/com/gin/springboot3template/sys/dao/RelationRolePermissionDao.java
+++ src/main/java/com/gin/springboot3template/sys/dao/RelationRolePermissionDao.java
@@ -4,14 +4,14 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gin.springboot3template.sys.config.redis.RedisMybatisCache;
 import com.gin.springboot3template.sys.entity.RelationRolePermission;
 import org.apache.ibatis.annotations.CacheNamespace;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author : ginstone
  * @version : v1.0.0
  * @since : 2022/12/14 16:56
  */
-@Repository
+@Mapper
 @CacheNamespace(flushInterval = 5L * 60 * 1000, implementation = RedisMybatisCache.class)
 public interface RelationRolePermissionDao extends BaseMapper<RelationRolePermission> {
 }
