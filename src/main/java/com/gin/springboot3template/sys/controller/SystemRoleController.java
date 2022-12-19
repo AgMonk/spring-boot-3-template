aa7bf9d267167909b23f881234f8bfd5656811da
ginstone <hjg719@139.com>
2022-12-19 17:39:27 +0800

    授权管理框架 - 用户管理接口:分页查询角色;添加角色

--- src/main/java/com/gin/springboot3template/sys/controller/SystemRoleController.java
+++ src/main/java/com/gin/springboot3template/sys/controller/SystemRoleController.java
@@ -2,7 +2,9 @@ package com.gin.springboot3template.sys.controller;
 
 import com.gin.springboot3template.sys.annotation.MyRestController;
 import com.gin.springboot3template.sys.bo.Constant;
+import com.gin.springboot3template.sys.dto.form.SystemRoleForm;
 import com.gin.springboot3template.sys.dto.param.SystemRolePageParam;
+import com.gin.springboot3template.sys.response.Res;
 import com.gin.springboot3template.sys.response.ResPage;
 import com.gin.springboot3template.sys.service.RolePermissionService;
 import com.gin.springboot3template.sys.service.SystemRoleService;
@@ -16,6 +18,10 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 角色接口
@@ -34,13 +40,20 @@ public class SystemRoleController {
     public static final String API_PREFIX = "/sys/role";
     private final SystemRoleService systemRoleService;
     private final RolePermissionService rolePermissionService;
-    /* todo 添加角色 修改角色 删除角色 分页查询角色 查询所有角色 通过关键字检索角色*/
+    /* todo 添加角色 修改角色 删除角色  查询所有角色 通过关键字检索角色*/
+
+    @GetMapping("add")
+    @Operation(summary = "添加角色", description = "返回添加完成的角色")
+    @PreAuthorize(Constant.PRE_AUTHORITY_URI_OR_ADMIN)
+    public Res<List<SystemRoleVo>> add(@RequestBody @Validated List<SystemRoleForm> param, HttpServletRequest request) {
+        final List<SystemRoleVo> data = systemRoleService.addByParam(param).stream().map(SystemRoleVo::new).collect(Collectors.toList());
+        return Res.of(data);
+    }
 
     @GetMapping("page")
-    @Operation(summary = "分页查询")
+    @Operation(summary = "分页查询角色")
     @PreAuthorize(Constant.PRE_AUTHORITY_URI_OR_ADMIN)
     public ResPage<SystemRoleVo> page(@ParameterObject @Validated SystemRolePageParam pageParam, HttpServletRequest request) {
-        // todo 添加筛选条件: 持有指定角色的用户
         return systemRoleService.pageByParam(pageParam, SystemRoleVo::new);
     }
 
--- src/main/java/com/gin/springboot3template/sys/controller/TestController.java
+++ src/main/java/com/gin/springboot3template/sys/controller/TestController.java
@@ -1,25 +1,9 @@
 package com.gin.springboot3template.sys.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gin.springboot3template.sys.annotation.MyRestController;
-import com.gin.springboot3template.sys.base.BasePageParam;
-import com.gin.springboot3template.sys.bo.Constant;
-import com.gin.springboot3template.sys.entity.SystemUser;
-import com.gin.springboot3template.sys.response.Res;
-import com.gin.springboot3template.sys.service.SystemUserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 /**
@@ -33,37 +17,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class TestController {
 
-    private final SystemUserService systemUserService;
-
-    @GetMapping("page")
-    @Operation(summary = "分页查询")
-    @PreAuthorize("hasPermission('admin','角色','访问')")
-    public Res<Void> test(@ParameterObject @Validated PageParam pageParam, HttpServletRequest request) {
-        System.out.println("pageParam.getId() = " + pageParam.getId());
-        return Res.of(null);
-    }
-
-    @GetMapping("page1")
-    @Operation(summary = "分页查询")
-    @PreAuthorize(Constant.PRE_AUTHORITY_URI_OR_ADMIN)
-    public Res<Void> test(Integer id, HttpServletRequest request) {
-        final SystemUser systemUser = systemUserService.getById(id);
-        System.out.println("systemUser = " + systemUser);
-        return Res.of(null);
-    }
-
-    @Getter
-    @Setter
-    @Schema(description = "测试查询参数")
-    public static class PageParam extends BasePageParam {
-        @Schema(description = "ID")
-        @NotNull
-//        @EntityId(service = SystemUserServiceImpl.class)
-        Integer id;
-
-        @Override
-        public void handleQueryWrapper(QueryWrapper<?> queryWrapper) {
-            System.out.println("id = " + id);
-        }
-    }
-}   
+}
