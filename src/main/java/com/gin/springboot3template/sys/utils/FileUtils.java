3be0753723cec37c27de3e53c72b987ef1d43553
ginstone <hjg719@139.com>
2022-12-23 10:30:01 +0800

    工具类 - 文件: 移动,删除

--- src/main/java/com/gin/springboot3template/sys/utils/FileUtils.java
+++ src/main/java/com/gin/springboot3template/sys/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package com.gin.springboot3template.sys.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -9,9 +11,11 @@ import java.io.IOException;
  * @version : v1.0.0
  * @since : 2022/12/22 17:16
  */
+@Slf4j
 public class FileUtils {
 
     public static final String DOT = ".";
+    public static final String PATH_DELIMITER = "/";
 
     /**
      * 递归创建目录
@@ -24,6 +28,7 @@ public class FileUtils {
         if (!dir.exists() && !dir.mkdirs()) {
             throw new IOException("目录创建失败: " + dir.getPath());
         }
+        log.info("创建目录:" + dir.getPath());
     }
 
     /**
@@ -58,6 +63,52 @@ public class FileUtils {
         if (!file.delete()) {
             throw new IOException("文件删除失败: " + file.getPath());
         }
+        log.info("已删除文件:" + file.getPath());
+    }
+
+    /**
+     * 移动文件
+     * @param src  源文件
+     * @param dest 目标文件
+     */
+    public static void move(File src, File dest) throws IOException {
+        assertExists(src);
+        assertNotExists(dest);
+        mkdir(dest.getParentFile());
+        if (!src.renameTo(dest)) {
+            throw new IOException(String.format("移动失败 %s -> %s", src.getPath(), dest.getPath()));
+        }
+        log.info("已移动文件 {} -> {}", src.getPath(), dest.getPath());
+    }
+
+    /**
+     * 将文件移动到一个指定目录
+     * @param src 源文件
+     * @param dir 目标目录
+     */
+    public static void move2Dir(File src, File dir) throws IOException {
+        move(src, new File(dir.getPath() + PATH_DELIMITER + src.getName()));
+    }
+
+    /**
+     * 递归删除一个目录
+     * @param dir 目录
+     */
+    public static void deleteDir(File dir) {
+        //todo
+    }
+
+    /**
+     * 根据file是目录还是文件调用不同的删除方法
+     * @param file 目录或文件
+     */
+    public static void delete(File file) throws IOException {
+        assertExists(file);
+        if (file.isDirectory()) {
+            deleteDir(file);
+        } else {
+            deleteFile(file);
+        }
     }
 
     /**
@@ -80,4 +131,17 @@ public class FileUtils {
             throw new IOException("文件不存在");
         }
     }
+
+    /**
+     * 断言文件不存在
+     * @param file 文件
+     */
+    private static void assertNotExists(File file) throws IOException {
+        assertNotNull(file);
+        if (!file.exists()) {
+            throw new IOException("文件已存在: " + file.getPath());
+        }
+    }
+
+
 }   
