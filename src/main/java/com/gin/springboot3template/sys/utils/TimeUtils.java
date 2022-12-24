04385303af5cb43ef3622e018a670084bc751aa4
ginstone <hjg719@139.com>
2022-12-24 10:44:42 +0800

    工具类 - 时间:格式化和解析

--- src/main/java/com/gin/springboot3template/sys/utils/TimeUtils.java
+++ src/main/java/com/gin/springboot3template/sys/utils/TimeUtils.java
@@ -1,5 +1,9 @@
 package com.gin.springboot3template.sys.utils;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 import static java.util.concurrent.TimeUnit.*;
 
 /**
@@ -9,10 +13,120 @@ import static java.util.concurrent.TimeUnit.*;
  * @since : 2022/12/23 11:14
  */
 public class TimeUtils {
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final ZoneId DEFAULT_ZONE_ID = ZoneId.systemDefault();
+    public static final DateTimeFormatter FULL_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    public static final DateTimeFormatter MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
     /**
      * 千
      */
     private final static long K = 1000L;
+
+    /**
+     * 按格式输出一个日期时间
+     * @param zdt ZonedDateTime
+     * @return 日期时间
+     */
+    public static String format(ZonedDateTime zdt) {
+        return format(zdt, DATE_TIME_FORMATTER);
+    }
+
+    /**
+     * 按格式输出一个日期时间
+     * @param formatter 格式
+     * @param zdt       ZonedDateTime
+     * @return 日期时间
+     */
+    public static String format(ZonedDateTime zdt, DateTimeFormatter formatter) {
+        return formatter.format(zdt);
+    }
+
+    /**
+     * 按格式输出一个日期时间(单位:秒)
+     * @param time 时间戳
+     * @return 日期时间
+     */
+    public static String format(long time) {
+        return format(time, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 按格式输出一个日期时间
+     * @param time     时间戳
+     * @param timeUnit 时间单位
+     * @return 日期时间
+     */
+    public static String format(long time, TimeUnit timeUnit) {
+        return format(time, timeUnit, DATE_TIME_FORMATTER);
+    }
+
+    /**
+     * 按格式输出一个日期时间
+     * @param time      时间戳
+     * @param timeUnit  时间单位
+     * @param formatter 输出格式
+     * @return 日期时间
+     */
+    public static String format(long time, TimeUnit timeUnit, DateTimeFormatter formatter) {
+        return format(time, timeUnit, formatter, DEFAULT_ZONE_ID);
+    }
+
+    /**
+     * 按格式输出一个日期时间
+     * @param time      时间戳
+     * @param timeUnit  时间单位
+     * @param formatter 输出格式
+     * @param zoneId    地区id
+     * @return 日期时间
+     */
+    public static String format(long time, TimeUnit timeUnit, DateTimeFormatter formatter, ZoneId zoneId) {
+        final long sec = timeUnit.toSeconds(time);
+        return formatter.format(ZonedDateTime.ofInstant(Instant.ofEpochSecond(sec), zoneId));
+    }
+
+    /**
+     * 智能识别字符串到 ZonedDateTime
+     * @param string 字符串
+     * @return ZonedDateTime
+     */
+    public static ZonedDateTime parse(String string) {
+        return parse(string, DEFAULT_ZONE_ID);
+    }
+
+    /**
+     * 智能识别字符串到 ZonedDateTime
+     * @param string 字符串
+     * @return ZonedDateTime
+     */
+    public static ZonedDateTime parse(String string, ZoneId zoneId) {
+        try {
+            final LocalDateTime parse = LocalDateTime.parse(string, FULL_FORMATTER);
+            return ZonedDateTime.ofLocal(parse, zoneId, null);
+        } catch (Exception ignored) {
+        }
+        try {
+            final LocalDateTime parse = LocalDateTime.parse(string, DATE_TIME_FORMATTER);
+            return ZonedDateTime.ofLocal(parse, zoneId, null);
+        } catch (Exception ignored) {
+        }
+        try {
+            final LocalDate parse = LocalDate.parse(string, DATE_FORMATTER);
+            return ZonedDateTime.of(parse, LocalTime.MIN, zoneId);
+        } catch (Exception ignored) {
+        }
+        try {
+            final LocalDate parse = LocalDate.parse(string, MONTH_FORMATTER);
+            return ZonedDateTime.of(parse, LocalTime.MIN, zoneId);
+        } catch (Exception ignored) {
+        }
+        try {
+            return ZonedDateTime.parse(string);
+        } catch (Exception ignored) {
+        }
+
+        throw new RuntimeException("无法识别的日期格式:" + string);
+    }
 
     /**
      * 返回简单格式的时间间隔
@@ -54,5 +168,4 @@ public class TimeUtils {
         final long m = minutes - DAYS.toMinutes(d) - HOURS.toMinutes(h);
         return String.format("%dd%dh%dm", d, h, m);
     }
-
 }   
