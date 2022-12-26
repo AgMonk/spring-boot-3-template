d74549a612aeda741659fea5bf1c438a49c3be32
ginstone <hjg719@139.com>
2022-12-26 14:08:37 +0800

    附件服务 - 查询附件

--- src/main/java/com/gin/springboot3template/sys/base/BaseAttach.java
+++ src/main/java/com/gin/springboot3template/sys/base/BaseAttach.java
@@ -3,9 +3,7 @@ package com.gin.springboot3template.sys.base;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Comment;
 
@@ -19,25 +17,42 @@ import org.hibernate.annotations.Comment;
 @Setter
 @MappedSuperclass
 @Schema(description = "基础附件对象")
-@NoArgsConstructor
-@AllArgsConstructor
 public class BaseAttach extends BasePo {
-    @Column(length = 100, nullable = false)
+    @Column(length = 10)
+    @Comment("文件后缀")
+    @Schema(description = "文件后缀")
+    String ext;
+    @Column(length = 300, nullable = false, unique = true)
+    @Comment("文件路径")
+    @Schema(description = "文件路径")
+    String filePath;
+    @Column(length = 100)
     @Comment("文件原名称")
+    @Schema(description = "文件原名称")
     String originalFilename;
     @Column
     @Comment("附件所有者Id")
     @Schema(description = "附件所有者Id")
     Long ownerId;
-    @Column(length = 300, nullable = false, unique = true)
-    @Comment("文件路径")
-    String filePath;
-    @Column(length = 10, nullable = false)
-    @Comment("文件后缀")
-    String ext;
-    @Column(length = 100, nullable = false)
+    @Column(length = 200, nullable = false)
     @Comment("备注")
+    @Schema(description = "备注")
     String remark;
+    @Column
+    @Comment("上传者Id")
+    @Schema(description = "上传者Id")
+    Long uploaderId;
 
+    /**
+     * 写入所有者id / 备注 / 上传者id
+     * @param ownerId    附件所有者Id
+     * @param remark     备注
+     * @param uploaderId 上传者Id
+     */
+    public BaseAttach(long uploaderId, Long ownerId, String remark) {
+        this.ownerId = ownerId;
+        this.remark = remark;
+        this.uploaderId = uploaderId;
+    }
 
 }
--- src/main/java/com/gin/springboot3template/sys/service/AttachmentService.java
+++ src/main/java/com/gin/springboot3template/sys/service/AttachmentService.java
@@ -1,16 +1,26 @@
 package com.gin.springboot3template.sys.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.gin.springboot3template.sys.base.BaseAttach;
 import com.gin.springboot3template.sys.config.SystemProperties;
+import com.gin.springboot3template.sys.exception.BusinessException;
 import com.gin.springboot3template.sys.utils.FileUtils;
 import com.gin.springboot3template.sys.utils.SpringContextUtils;
 import com.gin.springboot3template.sys.utils.TimeUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.gin.springboot3template.sys.utils.FileUtils.PATH_DELIMITER;
 
@@ -22,18 +32,11 @@ import static com.gin.springboot3template.sys.utils.FileUtils.PATH_DELIMITER;
  * @since : 2022/12/24 13:45
  */
 public interface AttachmentService<T extends BaseAttach> extends MyService<T> {
+    String OWNER_ID = "owner_id";
     /**
      * 附件目录名
      */
     String PATH = "attach";
-
-    /**
-     * 创建附件对象
-     * @param multipartFile 上传的文件
-     * @param ownerId       所有者id
-     * @return 附件对象
-     */
-    T createAttach(MultipartFile multipartFile, Long ownerId);
 
     /**
      * 附件在根目录下保存的目录名 默认使用 "/attach" + 当天日期
@@ -50,7 +53,7 @@ public interface AttachmentService<T extends BaseAttach> extends MyService<T> {
      */
     default void configOwnerId(long ownerId, Set<Long> attachId) {
         final UpdateWrapper<T> uw = new UpdateWrapper<>();
-        uw.set("owner_id", ownerId).in("id", attachId);
+        uw.set(OWNER_ID, ownerId).in("id", attachId);
         update(uw);
     }
 
@@ -63,31 +66,64 @@ public interface AttachmentService<T extends BaseAttach> extends MyService<T> {
     }
 
     /**
-     * 保存附件文件
-     * @param multipartFile 上传的附件
-     * @param entity        实体对象
-     * @throws IOException 异常
+     * 根据所有者id 查询附件
+     * @param ownerId 所有者id
+     * @return 附件
      */
-    default void saveMultipartFile(MultipartFile multipartFile, T entity) throws IOException {
-        final File destFile = new File(homePath() + attachPath() + entity.getFilePath());
-        FileUtils.mkdir(destFile.getParentFile());
-        multipartFile.transferTo(destFile);
+    default List<T> listByOwnerId(long ownerId) {
+        final QueryWrapper<T> qw = new QueryWrapper<>();
+        qw.eq(OWNER_ID, ownerId);
+        return list(qw);
+    }
+
+    /**
+     * 根据所有者id 查询附件
+     * @param ownerId 所有者id
+     * @return 附件
+     */
+    default Map<Long, List<T>> listByOwnerId(Collection<Long> ownerId) {
+        final QueryWrapper<T> qw = new QueryWrapper<>();
+        qw.in(OWNER_ID, ownerId);
+        return list(qw).stream().collect(Collectors.groupingBy(BaseAttach::getOwnerId));
     }
 
     /**
      * 上传一个附件
-     * @param file    上传的文件
-     * @param ownerId 所有者id
+     * @param file   上传的文件
+     * @param entity 附件对象
      * @return 附件对象
      * @throws IOException 异常
      */
-    default T upload(MultipartFile file, Long ownerId) throws IOException {
-        final T entity = createAttach(file, ownerId);
-        saveMultipartFile(file, entity);
+    @Transactional(rollbackFor = Exception.class)
+    default T upload(@NotNull MultipartFile file, T entity) throws IOException {
+        writeFilePath(file, entity);
+        //保存附件
+        final File destFile = new File(homePath() + entity.getFilePath());
+        FileUtils.mkdir(destFile.getParentFile());
+        file.transferTo(destFile);
+        //写入数据库
         save(entity);
         return entity;
     }
 
-//    todo 查询附件
+    /**
+     * 生成文件名 , 写入后缀信息 / 原文件名信息 / 文件保存路径
+     * @param file   上传的文件
+     * @param entity 附件对象
+     */
+    default void writeFilePath(@NotNull MultipartFile file, T entity) {
+        final String oName = file.getOriginalFilename();
+        if (ObjectUtils.isEmpty(oName)) {
+            throw BusinessException.of(HttpStatus.BAD_REQUEST, "必须提供原文件名");
+        }
+        final String ext = FileUtils.getFileExtName(oName);
+        entity.setOriginalFilename(oName);
+        entity.setExt(ext);
+
+        //生成文件路径
+
+
+    }
+
 //    todo 删除附件
 }
