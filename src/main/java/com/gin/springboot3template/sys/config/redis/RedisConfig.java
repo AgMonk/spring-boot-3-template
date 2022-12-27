b67871427627badd1d7fb64397d12af281d3837a
ginstone <hjg719@139.com>
2022-12-27 17:59:18 +0800

    Redis - 使用 RedisTemplate 构造器:jsonTemplate / stringTemplate

--- src/main/java/com/gin/springboot3template/sys/config/redis/RedisConfig.java
+++ src/main/java/com/gin/springboot3template/sys/config/redis/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.gin.springboot3template.sys.config.redis;
 
-import com.gin.springboot3template.sys.utils.SpringContextUtils;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,34 +24,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     private final RedisTemplate<Object, Object> redisTemplate;
 
-    public static <T> RedisTemplate<String, T> createRedisJsonTemplate(String name) {
-        log.info("创建 RedisTemplate:" + name);
-        RedisTemplate<String, T> template = new RedisTemplate<>();
-        template.setConnectionFactory(SpringContextUtils.getContext().getBean(RedisConnectionFactory.class));
-        //String的序列化
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-
-    public static RedisTemplate<String, Object> createRedisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(SpringContextUtils.getContext().getBean(RedisConnectionFactory.class));
-        //String的序列化
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
+    @Bean
+    public RedisTemplate<String, Object> jsonTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisTemplateBuilder<String, Object>(redisConnectionFactory)
+                .setValueSerializer(new GenericJackson2JsonRedisSerializer())
+                .setHashValueSerializer(new GenericJackson2JsonRedisSerializer())
+                .build();
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisJsonTemplate(RedisConnectionFactory redisConnectionFactory) {
-        return createRedisJsonTemplate("json");
+    public RedisTemplate<String, String> stringTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisTemplateBuilder<String, String>(redisConnectionFactory)
+                .setValueSerializer(new StringRedisSerializer())
+                .setHashValueSerializer(new StringRedisSerializer())
+                .build();
     }
 
+    /**
+     * 修改默认Template的key序列化方式
+     */
     @PostConstruct
     void init() {
         log.info("{}", redisTemplate);
@@ -62,14 +52,17 @@ public class RedisConfig {
 
     /**
      * RedisTemplate 构造器
+     * key的序列化方式默认为string
      * @param <K> Key类型
      * @param <V> Value类型
      */
     public static class RedisTemplateBuilder<K, V> {
-        final RedisTemplate<K, V> template = new RedisTemplate<>();
+        private final RedisTemplate<K, V> template = new RedisTemplate<>();
 
         public RedisTemplateBuilder(RedisConnectionFactory redisConnectionFactory) {
             template.setConnectionFactory(redisConnectionFactory);
+            template.setHashKeySerializer(new StringRedisSerializer());
+            template.setKeySerializer(new StringRedisSerializer());
         }
 
         public RedisTemplate<K, V> build() {
