6f24cb06df6862515feb5d5597c954ea5b47db0b
ginstone <hjg719@139.com>
2023-01-19 15:33:53 +0800

    枚举 - 性别

---
+++ src/main/java/com/gin/springboot3template/sys/enums/Gender.java
@@ -0,0 +1,22 @@
+package com.gin.springboot3template.sys.enums;
+
+/**
+ * 性别
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2023/1/19 15:31
+ */
+public enum Gender {
+    /**
+     * 男
+     */
+    male,
+    /**
+     * 女
+     */
+    female,
+    /**
+     * 秘密
+     */
+    secret;
+}
