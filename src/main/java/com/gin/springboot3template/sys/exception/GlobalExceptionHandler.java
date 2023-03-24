0ec498446587029c7f67f297324f7f40341fa494
ginstone <hjg719@139.com>
2023-03-24 09:29:16 +0800

    统一异常处理 - 优化

--- src/main/java/com/gin/springboot3template/sys/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gin/springboot3template/sys/exception/GlobalExceptionHandler.java
@@ -19,9 +19,8 @@ import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.ArrayList;
@@ -35,18 +34,16 @@ import java.util.stream.Collectors;
  * @since : 2022/12/10 11:16
  */
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     private final SystemPermissionService systemPermissionService;
 
-    @ResponseBody
     @ExceptionHandler({CookieTheftException.class})
     public ResponseEntity<Res<Void>> exceptionHandler(CookieTheftException e) {
         return new ResponseEntity<>(Res.of(null, e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ResponseBody
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<Res<String>> exceptionHandler(AccessDeniedException e, @SuppressWarnings("unused") HttpServletRequest request) {
         log.warn(e.getLocalizedMessage());
@@ -55,7 +52,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(Res.of(message, Constant.Messages.ACCESS_DENIED), HttpStatus.FORBIDDEN);
     }
 
-    @ResponseBody
     @ExceptionHandler({HttpMediaTypeNotSupportedException.class})
     public ResponseEntity<Res<String>> exceptionHandler(
             HttpMediaTypeNotSupportedException e,
@@ -70,14 +66,12 @@ public class GlobalExceptionHandler {
      * @param e 其他异常
      * @return 处理
      */
-    @ResponseBody
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Res<?>> exceptionHandler(Exception e) {
         e.printStackTrace();
         return new ResponseEntity<>(Res.of(null, "服务器错误 请通知管理员"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ResponseBody
     @ExceptionHandler({HttpMessageNotReadableException.class})
     public ResponseEntity<Res<?>> exceptionHandler(HttpMessageNotReadableException e) {
         if (e.getCause() instanceof UnrecognizedPropertyException ee) {
@@ -91,13 +85,11 @@ public class GlobalExceptionHandler {
         return exceptionHandler((Exception) e);
     }
 
-    @ResponseBody
     @ExceptionHandler({MissingServletRequestParameterException.class})
     public ResponseEntity<Res<String>> exceptionHandler(MissingServletRequestParameterException e) {
         return new ResponseEntity<>(Res.of(e.getLocalizedMessage(), "请求缺少必须的参数"), HttpStatus.BAD_REQUEST);
     }
 
-    @ResponseBody
     @ExceptionHandler({MaxUploadSizeExceededException.class})
     public ResponseEntity<Res<String>> exceptionHandler(MaxUploadSizeExceededException e) {
         return new ResponseEntity<>(Res.of("文件大小超出限制", e.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
@@ -108,7 +100,6 @@ public class GlobalExceptionHandler {
      * @param e 阐述校验异常
      * @return 处理
      */
-    @ResponseBody
     @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<Res<String>> exceptionHandler(ConstraintViolationException e) {
         return new ResponseEntity<>(Res.of(e.getLocalizedMessage(), "参数校验异常"), HttpStatus.BAD_REQUEST);
@@ -119,14 +110,12 @@ public class GlobalExceptionHandler {
      * @param e 业务异常
      * @return 处理
      */
-    @ResponseBody
     @ExceptionHandler({BusinessException.class})
     public ResponseEntity<Res<List<String>>> exceptionHandler(BusinessException e) {
         log.warn("code: {} , 业务异常: {} , value: {}", e.getHttpStatus().value(), e.getTitle(), e.getMessages());
         return new ResponseEntity<>(Res.of(e.getMessages(), e.getTitle()), e.getHttpStatus());
     }
 
-    @ResponseBody
     @ExceptionHandler({BindException.class})
     public ResponseEntity<Res<?>> exceptionHandler(BindException e) {
         final List<MyFieldError> fieldErrors = e.getFieldErrors().stream().map(MyFieldError::new).collect(Collectors.toList());
@@ -141,7 +130,6 @@ public class GlobalExceptionHandler {
      * @param e 其他异常
      * @return 处理
      */
-    @ResponseBody
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
     public ResponseEntity<Res<String>> exceptionHandler(HttpRequestMethodNotSupportedException e) {
         return new ResponseEntity<>(Res.of(e.getLocalizedMessage(), "不支持的请求方法"), HttpStatus.BAD_REQUEST);
